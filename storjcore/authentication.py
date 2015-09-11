3af9226c705b495bbb9e9cfb8e007a8519adf51d
F483 <fabian.barkhau@gmail.com>
2015-09-11 19:17:34 +0200

    added authentication header validation

--- storjcore/authentication.py
+++ storjcore/authentication.py
@@ -1,11 +1,53 @@
-import datetime
 import email.utils
 import time
+from email.utils import parsedate_tz
+from email.utils import mktime_tz
+from datetime import datetime
+from datetime import timedelta
+from storjcore import validate
 
 
-def create_headers(btctxstore, remote_address, authentication_wif):
-    timeval = time.mktime(datetime.datetime.now().timetuple())
+class AuthenticationError(Exception):
+    pass
+
+
+def create_headers(btctxstore, recipient_address, authentication_wif):
+
+    # validate input
+    btctxstore = validate.is_btctxstore(btctxstore)
+    recipient_address = validate.is_btcaddress(recipient_address)
+    authentication_wif = validate.is_btcwif(authentication_wif)
+
+    # create header date and signature
+    timeval = time.mktime(datetime.now().timetuple())
     date = email.utils.formatdate(timeval=timeval, localtime=True, usegmt=True)
-    msg = remote_address + " " + date
+    msg = recipient_address + " " + date
     signature = btctxstore.sign_unicode(authentication_wif, msg)
     return {"Date": date, "Authorization": signature}
+
+
+def validate_headers(btctxstore, headers, timeout_sec,
+                     sender_address, recipient_address):
+    # validate input
+    btctxstore = validate.is_btctxstore(btctxstore)
+    headers = validate.is_dict(headers)
+    signature = validate.is_signature(headers.get("Authorization"))
+    date = validate.is_rfc7231_date(headers.get("Date"))
+    timeout_sec = validate.is_unsigned_int(timeout_sec)
+    sender_address = validate.is_btcaddress(sender_address)
+    recipient_address = validate.is_btcaddress(recipient_address)
+
+    # verify date
+    clientdate = datetime.fromtimestamp(mktime_tz(parsedate_tz(date)))
+    timeout = timedelta(seconds=timeout_sec)
+    delta = abs(datetime.now() - clientdate)
+    if delta >= timeout:
+        msg = "Invalid header date {0} >= {1}!".format(delta, timeout)
+        raise AuthenticationError(msg)
+
+    # verify signature
+    msg = recipient_address + " " + date
+    if not btctxstore.verify_signature_unicode(sender_address, signature, msg):
+        msg = "Invalid signature for auth addr {0}!".format(sender_address)
+        raise AuthenticationError(msg)
+    return True
---
+++ storjcore/validate.py
@@ -0,0 +1,40 @@
+
+
+class ValidationError(Exception):
+    pass
+
+
+def is_unsigned_int(i):
+    # FIXME implement
+    return i
+
+
+def is_dict(d):
+    if not isinstance(d, dict):
+        raise ValidationError()
+    return d
+
+
+def is_signature(s):
+    # FIXME implement
+    return s
+
+
+def is_rfc7231_date(d):
+    # FIXME implement
+    return d
+
+
+def is_btcaddress(address):
+    # FIXME implement
+    return address
+
+
+def is_btcwif(wif):
+    # FIXME implement
+    return wif
+
+
+def is_btctxstore(btctxstore):
+    # FIXME implement
+    return btctxstore
